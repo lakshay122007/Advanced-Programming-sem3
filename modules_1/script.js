0b62f7567a45b64bb4bba7f6a0e8da6a6bee1390
lakshay122007 <lakshay11goyal@gmail.com>
2026-08-19 09:28:05 +0530

    require statement

---
+++ modules_1/script.js
@@ -0,0 +1,7 @@
+// es5/common js
+//es6 module
+
+//eg - 
+
+const time = "9 am";
+module.exports = time;
---
+++ modules_1/test.js
@@ -0,0 +1,4 @@
+//we use require function to import modules in common js -
+
+require("./script")
+
