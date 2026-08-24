a5a9fe60fa2bdc867d075f343953289b1f184e79
lakshay122007 <lakshay11goyal@gmail.com>
2026-08-24 09:58:30 +0530

    fs csv file

--- modules_1/script.js
+++ modules_1/script.js
@@ -1,24 +1,35 @@
-// // es5/common js
-// //es6 module
-// //inbuilt module
+// // // es5/common js
+// // //es6 module
+// // //inbuilt module
 
-// //eg - 
+// // //eg - 
 
-// const time = "9 am";
+// // const time = "9 am";
 
-// console.log(time);
-// console.log(time);
+// // console.log(time);
+// // console.log(time);
 
-// module.exports = time;
+// // module.exports = time;
 
-//Event Emitters
+// //Event Emitters
 
-const EventEmitter = require('events')
-// console.log(EventEmitter)
+// const EventEmitter = require('events')
+// // console.log(EventEmitter)
 
-const celebrity = new EventEmitter()
-celebrity.on("New video",() => { //on is a listener
-    console.log("Heyy good good")
-})
+// const celebrity = new EventEmitter()
+// celebrity.on("New video",() => { //on is a listener
+//     console.log("Heyy good good")
+// })
 
-celebrity.emit("New video") //fire an event
+// celebrity.emit("New video") //fire an event
+
+//fs-table
+
+const fs = require("fs")
+
+fs.writeFileSync("big_data.csv", "id,userName,orderValue\n");
+
+for (let i = 1; i<=3000; i++){
+    fs.appendFileSync("big_data.csv", `${i}, username${i}, ${Math.random()*100}`)
+}
+
