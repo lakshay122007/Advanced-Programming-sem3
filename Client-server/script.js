f7cac0c2495e547fcc258310d3d8dc1077100d4e
lakshay122007 <lakshay11goyal@gmail.com>
2026-08-26 10:21:21 +0530

    Methods

--- Client-server/script.js
+++ Client-server/script.js
@@ -5,13 +5,13 @@ const server = http.createServer((req,res) => {
     // console.log(req)
     // console.log(res)
 
-    // res.write("yo yo")
-    res.writeHead(200, {
-        'Content-type': "text/html"
-    })
-    res.end("<i>bye</i>")
+    res.write("yo yo")
+    // res.writeHead(200, {
+    //     'Content-type': "text/html"
+    // })
+    res.end()
     // res.statusCode
-    res.setHeader()
+    // res.setHeader()
 })
 server.listen(3000, () => {
     console.log("YES YES")
@@ -20,3 +20,7 @@ server.listen(3000, () => {
 
 
 
+// http.createServer((req, res) => {
+//     res.end("Response Complete");
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+// }).listen(3000);
