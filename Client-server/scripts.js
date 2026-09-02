b4035d9779137bfb3c53322161a010236dfa1e6d
lakshay122007 <lakshay11goyal@gmail.com>
2026-09-02 09:18:39 +0530

    changes

--- Client-server/scripts.js
+++ Client-server/scripts.js
@@ -1,27 +1,26 @@
-const http = require("http")
-const {users,posts} = require("./data.js")
+const http = require("http");
+const { users, posts } = require("./data.js");
 
 // console.log(users);
 
-const server = http.createServer((req,res) => {
-    console.log(req.url)
-    console.log(req.method)
-    const path = req.url;
-    const arr = path.split("/")
-    const id = Number(arr[2])
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  console.log(req.method);
+  const path = req.url;
+  const arr = path.split("/");
+  const id = Number(arr[2]);
 
-    res.writeHead(200, {
-        'Content-Type' : "application/json"
-    })
-    if(req.url == "/users"){
-        res.end(JSON.stringify(users));
-    }
-    else if(req.url == `/users/${id}`){
-        const obj = users.find((obj) => obj.id === id)
-        res.end(JSON.stringify(obj))
-    }
-})
+  res.writeHead(200, {
+    "Content-Type": "application/json",
+  });
+  if (req.url == "/users") {
+    res.end(JSON.stringify(users));
+  } else if (req.url == `/users/${id}`) {
+    const obj = users.find((obj) => obj.id === id);
+    res.end(JSON.stringify(obj));
+  }
+});
 
 server.listen(3000, () => {
-    console.log("server is running 🏃")
-})
+  console.log("server is running 🏃");
+});
